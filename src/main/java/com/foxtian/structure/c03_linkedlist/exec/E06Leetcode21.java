11fd9bc63f1a61752253e7e197d83174afefdb2f
狐狸半面添 <3179167073@qq.com>
2024-08-07 21:14:18 +0800

    :art: 合并两个有序列表

--- src/main/java/com/foxtian/structure/c03_linkedlist/exec/E06Leetcode21.java
+++ src/main/java/com/foxtian/structure/c03_linkedlist/exec/E06Leetcode21.java
@@ -1,7 +1,7 @@
 package com.foxtian.structure.c03_linkedlist.exec;
 
 /**
- * Description:
+ * Description: <a href="https://leetcode.cn/problems/merge-two-sorted-lists/description/">21. 合并两个有序链表<a/>
  *
  * @Author 狐狸半面添
  * @Create 2024/8/6 21:29
@@ -9,75 +9,57 @@ package com.foxtian.structure.c03_linkedlist.exec;
  */
 public class E06Leetcode21 {
 
-    // 方法一：快慢指针
-    public static ListNode deleteDuplicates1(ListNode head) {
-        ListNode sentinel = new ListNode(-1, head);
-        ListNode p1 = sentinel;
-        ListNode p2;
-        while ((p2 = p1.next) != null) {
-            int count = 0;
-            int val = p2.val;
-            while (p2 != null && p2.val == val) {
-                p2 = p2.next;
-                count++;
-            }
-            if (count > 1) {
-                p1.next = p2;
+    // 方法一
+    public static ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
+        ListNode sentinel = new ListNode(-1, null);
+        ListNode p = sentinel;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                p.next = list1;
+                list1 = list1.next;
             } else {
-                p1 = p1.next;
+                p.next = list2;
+                list2 = list2.next;
             }
+            p = p.next;
         }
+
+        if (list1 != null) {
+            p.next = list1;
+        } else {
+            p.next = list2;
+        }
+
         return sentinel.next;
     }
 
-    // 方法二：递归
-    public static ListNode deleteDuplicates2(ListNode head) {
-        if (head == null) {
-            return null;
+    public static ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
         }
 
-        ListNode node = head;
-        while (node.next != null && node.next.val == head.val) {
-            node = node.next;
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists2(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists2(list1, list2.next);
+            return list2;
         }
-        if (head != node) {
-            return deleteDuplicates2(node.next);
-        }
-        head.next = deleteDuplicates2(head.next);
-        return head;
-    }
-
-    // 方法三：三个指针
-    public static ListNode deleteDuplicates3(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
-        }
-        ListNode sentinel = new ListNode(-1, head);
-        ListNode p1 = sentinel;
-        ListNode p2, p3;
-        while ((p2 = p1.next) != null && (p3 = p2.next) != null) {
-            if (p2.val == p3.val) {
-                while ((p3 = p3.next) != null && p3.val == p2.val) ;
-                p1.next = p3;
-            } else {
-                p1 = p1.next;
-            }
-        }
-
-        return sentinel.next;
     }
 
     public static void main(String[] args) {
-        ListNode node5_2 = new ListNode(5, null);
-        ListNode node5_1 = new ListNode(5, node5_2);
-        ListNode node4 = new ListNode(4, node5_1);
-        ListNode node3 = new ListNode(3, node4);
-        ListNode node2_2 = new ListNode(2, node3);
-        ListNode node2_1 = new ListNode(2, node2_2);
-        ListNode node1 = new ListNode(1, node2_1);
+        ListNode list1_4 = new ListNode(4, null);
+        ListNode list1_2 = new ListNode(2, list1_4);
+        ListNode list1_1 = new ListNode(1, list1_2);
 
-        print(node1);
-        ListNode newHead = deleteDuplicates3(node1);
+        ListNode list2_4 = new ListNode(4, null);
+        ListNode list2_3 = new ListNode(3, list2_4);
+        ListNode list2_1 = new ListNode(1, list2_3);
+
+        ListNode newHead = mergeTwoLists2(list1_1, list2_1);
         print(newHead);
     }
 
