7e6e3bd207612987907a0b5fcc983c40b34df477
狐狸半面添 <3179167073@qq.com>
2024-08-18 20:07:33 +0800

    :art: 101. 对称二叉树

--- src/main/java/com/foxtian/structure/c11_binarytree/exec/E02Leetcode94.java
+++ src/main/java/com/foxtian/structure/c11_binarytree/exec/E02Leetcode94.java
@@ -1,5 +1,8 @@
 package com.foxtian.structure.c11_binarytree.exec;
 
+import com.foxtian.structure.c11_binarytree.TreeNode;
+import org.junit.jupiter.api.BeforeAll;
+
 /**
  * Description:
  *
@@ -8,4 +11,28 @@ package com.foxtian.structure.c11_binarytree.exec;
  * @Version 1.0
  */
 public class E02Leetcode94 {
+    private static TreeNode root;
+
+    @BeforeAll
+    public static void init() {
+                /*
+                   1
+                  / \
+                 2  3
+                /  / \
+               4  5   6
+         */
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node3 = new TreeNode(3, node5, node6);
+
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2, node4, null);
+
+        TreeNode node1 = new TreeNode(1, node2, node3);
+
+        root = node1;
+    }
+
+
 }
---
+++ src/main/java/com/foxtian/structure/c11_binarytree/exec/E04Leetcode101.java
@@ -0,0 +1,50 @@
+package com.foxtian.structure.c11_binarytree.exec;
+
+import com.foxtian.structure.c11_binarytree.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Description: <a href="https://leetcode.cn/problems/symmetric-tree/description/">101. 对称二叉树</a>
+ *
+ * @Author 狐狸半面添
+ * @Create 2024/8/18 20:01
+ * @Version 1.0
+ */
+public class E04Leetcode101 {
+    public static boolean isSymmetric(TreeNode root) {
+        return dfs(root.left, root.right);
+    }
+
+    public static boolean dfs(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        } else {
+            if (left.val != right.val) {
+                return false;
+            }
+            return dfs(left.left, right.right) && dfs(left.right, right.left);
+        }
+    }
+
+    @Test
+    public void testIsSymmetric() {
+        TreeNode root1 = new TreeNode(
+                1,
+                new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+                new TreeNode(2, new TreeNode(4), new TreeNode(3))
+        );
+        assertTrue(isSymmetric(root1));
+
+        TreeNode root2 = new TreeNode(
+                1,
+                new TreeNode(2, null, new TreeNode(3)),
+                new TreeNode(2, null, new TreeNode(3))
+        );
+        assertFalse(isSymmetric(root2));
+    }
+}
