abddcb65cc23f0d5a3e3bd41a966456526324dd3
狐狸半面添 <3179167073@qq.com>
2024-08-20 00:07:43 +0800

    :art: 145. 二叉树的后序遍历

--- src/main/java/com/foxtian/structure/c11_binarytree/exec/E03Leetcode145.java
+++ src/main/java/com/foxtian/structure/c11_binarytree/exec/E03Leetcode145.java
@@ -1,10 +1,19 @@
 package com.foxtian.structure.c11_binarytree.exec;
 
 import com.foxtian.structure.c11_binarytree.TreeNode;
+import com.foxtian.utils.ListGenUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 /**
- * Description:
+ * Description: <a href="https://leetcode.cn/problems/binary-tree-postorder-traversal/description/">145. 二叉树的后序遍历</a>
  *
  * @Author 狐狸半面添
  * @Create 2024/8/17 22:58
@@ -34,5 +43,56 @@ public class E03Leetcode145 {
         root = node1;
     }
 
+    public static List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    public static void postorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        postorder(node.left, res);
+        postorder(node.right, res);
+
+        res.add(node.val);
+    }
+
+    @Test
+    public void testPostorderTraversal1() {
+        List<Integer> res = postorderTraversal1(root);
+        assertIterableEquals(ListGenUtils.toList(4, 2, 5, 6, 3, 1), res);
+    }
+
+    public static List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        TreeNode pop = null;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            } else {
+                TreeNode peek = stack.peek();
+                if (peek.right == null || peek.right == pop) {
+                    res.add(stack.pop().val);
+                    pop = peek;
+                } else {
+                    curr = peek.right;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    @Test
+    public void testPostorderTraversal2() {
+        List<Integer> res = postorderTraversal2(root);
+        assertIterableEquals(ListGenUtils.toList(4, 2, 5, 6, 3, 1), res);
+    }
 
 }
